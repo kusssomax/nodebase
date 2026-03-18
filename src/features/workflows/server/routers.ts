6b2e6e81c9d70a89bd0b89533ee9f509e5021c10
kusssomax <maxkusjo@gmail.com>
2026-03-18 08:46:26 +0200

    workflows pagination

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -3,6 +3,7 @@ import { createTRPCRouter } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
 import { z } from "zod";
 import prisma from "@/lib/db";
+import { PAGINATION } from "@/config/contants";
 
 export const workflowsRouter = createTRPCRouter({
   create: PremiumProcedure.mutation(async ({ ctx }) => {
@@ -51,11 +52,38 @@ export const workflowsRouter = createTRPCRouter({
     }),
 
     getMany: protectedProcedure
-    .query(async ({ ctx }) => {
-      return prisma.workflow.findMany({
-        where: {
-          userId: ctx.auth.user.id,
-        },
-      });
+    .input(z.object({
+      page: z.number().default(PAGINATION.DEFAULT_PAGE),
+      pageSize: z.number().min(PAGINATION.MIN_PAGE_SIZE).max(PAGINATION.MAX_PAGE_SIZE).default(PAGINATION.DEFAULT_PAGE_SIZE),
+      search: z.string().default(""),
+    }))
+    .query(async ({ ctx, input }) => {
+      
+      const { page, pageSize, search } = input;
+      const [items, totalCount] = await Promise.all([
+        prisma.workflow.findMany({
+          skip: (page - 1) * pageSize,
+          take: pageSize,
+          where: { userId: ctx.auth.user.id, name: { contains: search, mode: "insensitive" } },
+          orderBy: { updatedAt: "desc" },
+        }),
+        prisma.workflow.count({
+          where: { userId: ctx.auth.user.id, name: { contains: search, mode: "insensitive" } },
+        }),
+      ]);
+
+      const totalPages = Math.ceil(totalCount / pageSize);
+      const hasNextPage = page < totalPages;
+      const hasPreviousPage = page > 1;
+
+      return {
+        items,
+        page,
+        pageSize,
+        totalCount,
+        totalPages,
+        hasNextPage,
+        hasPreviousPage,
+      };
     }),
 });
