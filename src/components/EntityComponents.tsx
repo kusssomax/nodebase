8c3de25476d70192e78a038ed50037642824b7a7
kusssomax <maxkusjo@gmail.com>
2026-03-21 10:28:59 +0200

    workflows-ui

--- src/components/EntityComponents.tsx
+++ src/components/EntityComponents.tsx
@@ -1,8 +1,31 @@
-import { PlusIcon } from "lucide-react";
+import { Loader2Icon, MoreVerticalIcon, PackageOpenIcon, PlusIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, TriangleAlertIcon } from "lucide-react";
 import { Input } from "./ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import {
+  Card, 
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import {
+  Empty,
+  EmptyHeader,
+  EmptyTitle,
+  EmptyDescription,
+  EmptyContent,
+  EmptyMedia,
+} from "./ui/empty";
 
 type EntityHeaderProps = {
   title: string;
@@ -124,18 +147,18 @@ export const EntityPagination = ({
       </div>
       <div className="flex items-center  justify-end py-4 space-x-2">
         <Button
-        disabled={page === 1 || disabled}
-        variant="outline"
-        size="sm"
-        onClick={() => onPageChange(Math.max(1, page - 1))}
+          disabled={page === 1 || disabled}
+          variant="outline"
+          size="sm"
+          onClick={() => onPageChange(Math.max(1, page - 1))}
         >
           Previous
         </Button>
         <Button
-        disabled={page === totalPages || totalPages === 0 || disabled}
-        variant="outline"
-        size="sm"
-        onClick={() => onPageChange(Math.min(totalPages, page + 1))}
+          disabled={page === totalPages || totalPages === 0 || disabled}
+          variant="outline"
+          size="sm"
+          onClick={() => onPageChange(Math.min(totalPages, page + 1))}
         >
           Next
         </Button>
@@ -143,3 +166,160 @@ export const EntityPagination = ({
     </div>
   );
 };
+
+interface StateViewProps {
+  message?: string;
+}
+
+export const LoadingView = ({ message }: StateViewProps) => {
+  return (
+    <div className="flex items-center justify-center h-full flex-1 flex-col gap-y-4">
+      <Loader2Icon className="size-6 animate-spin text-primary" />
+      {!!message && <p className="text-sm text-muted-foreground">{message}</p>}
+    </div>
+  );
+};
+
+export const ErrorView = ({ message }: StateViewProps) => {
+  return (
+    <div className="flex items-center justify-center h-full flex-1 flex-col gap-y-4">
+      <TriangleAlertIcon className="size-6 text-primary" />
+      {!!message && <p className="text-sm text-muted-foreground">{message}</p>}
+    </div>
+  );
+};
+
+interface EntityViewProps extends StateViewProps {
+  onNew?: () => void;
+}
+
+export const EmptyView = ({ message, onNew }: EntityViewProps) => {
+  return (
+    <Empty className="border border-dashed bg-white">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <PackageOpenIcon />
+        </EmptyMedia>
+      </EmptyHeader>
+      <EmptyTitle>No Items</EmptyTitle>
+      {!!message && <EmptyDescription>{message}</EmptyDescription>}
+      {onNew && (
+        <EmptyContent>
+          <Button onClick={onNew} size="sm">
+            <PlusIcon className="size-4" />
+            Add item
+          </Button>
+        </EmptyContent>
+      )}
+    </Empty>
+  );
+};
+
+
+interface EntityListProps<T> {
+  items: T[];
+  renderItem: (item: T, index: number) => React.ReactNode;
+  getKey: (item: T, index: number) => string | number;
+  emptyView?: React.ReactNode;
+  className?: string;
+}
+
+export function EntityList<T>({ items, renderItem, getKey, emptyView, className="" }: EntityListProps<T>) {
+if (items.length === 0 && emptyView) {
+  return (
+    <div className={"flex-1 flex items-center justify-center"}>
+      <div className="max-w-sm mx-auto">
+        {emptyView}
+      </div>
+
+    </div>
+  )
+}
+  return (
+    <div className={cn("flex flex-col gap-y-4", className)}>
+      {
+        items.map((item, index) => (
+          <div
+          key={getKey ? getKey(item, index) : index}
+          >
+            {renderItem(item, index)}
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+ 
+
+interface EntityItemProps {
+  href: string;
+  title: string;
+  subtitle?: React.ReactNode;
+  image?: React.ReactNode;
+  actions?: React.ReactNode;
+  onRemove?: () => void | Promise<void>;
+  isRemoving?: boolean;
+  className?: string;
+}
+
+export const EntityItem = ({ href, title, subtitle, image, actions, onRemove, isRemoving, className="" }: EntityItemProps) => {
+
+  const handleRemove = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (isRemoving) return;
+
+    if (onRemove) {
+      await onRemove();
+    }
+  }
+
+  return (
+    <Link href={href} prefetch>
+      <Card className={cn("shadow-none p-4 hover:shadow cursor-pointer", isRemoving && "opacity-50 cursor-not-allowed", className)}>
+        <CardContent className="flex flex-row items-center justify-between p-0">
+          <div className="flex flex-items-center gap-3">
+            {image}
+            <div>
+              <CardTitle
+              className="text-base font-medium"
+              >{title}</CardTitle>
+              {subtitle && (
+                <CardDescription className="text-xs">
+                  {subtitle}
+                </CardDescription>
+              )}
+            </div>
+          </div>
+          {actions || onRemove && (
+            <div className="flex items-center gap-x-4">
+              {actions}
+              {!!onRemove && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                    >
+                      <MoreVerticalIcon className="size-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent side="bottom" align="end" onClick={(e) => e.stopPropagation()}>
+                    <DropdownMenuItem onClick={handleRemove} disabled={isRemoving}>
+                      <Trash2Icon className="size-4" />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </Link>
+  )
+}
+
+
+
--- src/features/workflows/hooks/useWorkflows.ts
+++ src/features/workflows/hooks/useWorkflows.ts
@@ -27,4 +27,22 @@ export const useCreateWorkflow = () => {
         }
     }))
 
+};
+
+export const useRemoveWorkflow = () => {
+    const trpc = useTRPC();
+    const queryClient = useQueryClient();
+
+    return useMutation(trpc.workflows.remove.mutationOptions({
+        onSuccess: (data) => {
+            toast.success(`Workflow ${data.name} deleted successfully`);
+            queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
+            queryClient.invalidateQueries(
+                trpc.workflows.getOne.queryFilter({ id: data.id })
+            )
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        }
+    })
 }
